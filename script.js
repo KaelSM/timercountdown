a06c8414df41c182a22db52e01e749fa50b2f228
Kael Moreira <113145289+KaelSM@users.noreply.github.com>
2023-06-22 16:45:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const countDown = () => {
+  const countDown = () => {
       // This code sets the date of the next election, and selects all flip cards.
 
-  const countDate = new Date("June 8, 2023 14:00:00").getTime(); // June 8, 2023 at 2 PM
+      const countDate = new Date("June 22, 2023 16:36:00").getTime();
       const flipCards = document.querySelectorAll(".flip-card");
+      let isCountdownFinished = false;
   
-      setInterval(updateCountdown, 1000);
-  
-      // This code updates the countdown on the screen based on the current time.
+// This code updates the countdown on the screen based on the current time.
 // It calculates the time left to the event by subtracting the current time from the event time.
 // It then calculates the number of days, hours, minutes, and seconds left to the event.
 // Finally, it updates the flip cards on the screen with the values for days, hours, minutes, and seconds.
+// It does this by calling the flip function for each flip card, and passing in the flip card and the new number to display.
+// It also checks to see if the time left to the event is less than or equal to zero.
+// If it is, it clears the interval, and displays the confetti and notification.
 
 function updateCountdown() {
-        const now = new Date().getTime();
-        const gap = countDate - now;
-  
-        const seconds = Math.floor((gap / 1000) % 60);
-        const minutes = Math.floor((gap / 1000 / 60) % 60);
-        const hours = Math.floor((gap / 1000 / 60 / 60) % 24);
-        const days = Math.floor(gap / 1000 / 60 / 60 / 24);
-        
-        flip(flipCards[0], Math.floor(days / 10));
-        flip(flipCards[1], days % 10);
-        flip(flipCards[2], Math.floor(hours / 10));
-        flip(flipCards[3], hours % 10);
-        flip(flipCards[4], Math.floor(minutes / 10));
-        flip(flipCards[5], minutes % 10);
-        flip(flipCards[6], Math.floor(seconds / 10));
-        flip(flipCards[7], seconds % 10);
-      }
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  if (gap <= 0 && !isCountdownFinished) {
+    clearInterval(interval);
+    isCountdownFinished = true;
+    displayConfettiAndNotification();
+    return;
+  }
+
+  const seconds = Math.floor((gap / 1000) % 60);
+  const minutes = Math.floor((gap / 1000 / 60) % 60);
+  const hours = Math.floor((gap / 1000 / 60 / 60) % 24);
+  const days = Math.floor(gap / 1000 / 60 / 60 / 24);
+
+  flip(flipCards[0], Math.floor(days / 10));
+  flip(flipCards[1], days % 10);
+  flip(flipCards[2], Math.floor(hours / 10));
+  flip(flipCards[3], hours % 10);
+  flip(flipCards[4], Math.floor(minutes / 10));
+  flip(flipCards[5], minutes % 10);
+  flip(flipCards[6], Math.floor(seconds / 10));
+  flip(flipCards[7], seconds % 10);
+}
       
 // This function is responsible for flipping the cards. 
 // It takes two arguments, the card to be flipped, and the new number to display
@@ -70,9 +79,20 @@ function flip(flipCard, newNumber) {
         });
         flipCard.append(topFlip, bottomFlip);
       }
-      
+
+      function displayConfettiAndNotification() {
+        const confetti = document.getElementById("confetti");
+        const notification = document.getElementById("notification");
+  
+        confetti.style.display = "block";
+        notification.style.display = "block";
+  
+        // Add your confetti animation code or use a library like confetti.js
+      }
+  
+      const interval = setInterval(updateCountdown, 1000);
       updateCountdown();
     };
-    
+  
     countDown();
-    });
+  });
